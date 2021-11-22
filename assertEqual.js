5bb84dfc29fa31e74da6a65688131bf75f57bf7f
chris lopes <chris.lopes@gmail.com>
2021-11-22 23:53:32 +0000

    created assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅✅✅ Assertion Passed ' + actual + ' === ' + expected);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Fry", "Fry");
+assertEqual(1, 2);
+assertEqual(1, 1);
