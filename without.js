7ae85f12307e46e24020b789f1751478c5adbeba
chris lopes <chris.lopes@gmail.com>
2021-11-24 01:35:16 +0000

    add without js

--- without.js
+++ without.js
@@ -13,21 +13,33 @@ const assertArraysEqual = function(arr1, arr2) {
   return;
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 const without = function(source, itemsToRemove) {
-  
+  let withoutArray = [];
+  // console.log(source, itemsToRemove);
+  if (itemsToRemove.length === 0) {
+    withoutArray = source;
+    return withoutArray;
+  }
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      withoutArray.push(source[i]);
+    }
+  }
+  return withoutArray;
 };
 
 // TEST
-without([1, 2, 3], [1]) // => [2, 3]
-without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+console.log(without([1, 2, 3], [])); // => [1, 2, 3]
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
 
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
