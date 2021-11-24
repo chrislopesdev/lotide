e8cc3f64b528ae9d1310118a26c0720ac47066db
chris lopes <chris.lopes@gmail.com>
2021-11-24 00:20:52 +0000

    add assertArraysEqual js

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+    return;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+      return;
+    }
+  }
+  console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  return;
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
