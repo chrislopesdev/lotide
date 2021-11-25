e07a8e28f54ff15559d8d7416529d1fff17e0d25
chris lopes <chris.lopes@gmail.com>
2021-11-25 11:51:55 +0000

    update eqObects

--- eqObjects.js
+++ eqObjects.js
@@ -8,16 +8,16 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
-    console.log(false);
+    // console.log(false);
     return false;
   }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      console.log(false);
+      // console.log(false);
       return false;
     }
   }
-  console.log(true);
+  // console.log(true);
   return true;
 };
 
