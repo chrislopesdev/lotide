408bce4e6ddeefd143b8ff7384e911fdb9f81e2c
chris lopes <chris.lopes@gmail.com>
2021-11-29 20:18:47 +0000

    modulize assertArraysEqual and middle

---
+++ test/middle.test.js
@@ -0,0 +1,15 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+// TEST CASES
+middle([1]); // => []
+middle([1, 2]); // => []
+
+middle([1, 2, 3]); // => [2]
+middle([1, 2, 3, 4, 5]); // => [3]
+
+middle([1, 2, 3, 4]); // => [2, 3]
+middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+
+console.log(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
+console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]));
