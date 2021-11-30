8262b20d1b637936c8a729e99940b38078504888
chris lopes <chris.lopes@gmail.com>
2021-11-30 12:14:02 +0000

    update letterPosition mocha chai

--- letterPositions.js
+++ letterPositions.js
@@ -1,22 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    console.log(false);
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      console.log(false);
-      return false;
-    }
-  }
-  console.log(true);
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  console.log(eqArrays(arr1, arr2) ? `✅✅✅ Assertion Passed: ${arr1} === ${arr2}` : `🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-};
-
 const letterPositions = function(sentence) {
   let results = {};
   let sentenceArr = sentence.split('');
@@ -30,16 +11,5 @@ const letterPositions = function(sentence) {
   }
   return results;
 };
-
-console.log(letterPositions('hello'));
-
-assertArraysEqual(letterPositions('hello'),
-  {
-    h: [0],
-    e: [1],
-    l: [2, 3],
-    o: [4]
-  }
-);
 
 module.exports = letterPositions;
---
+++ test/letterPositions.test.js
@@ -0,0 +1,15 @@
+const letterPositions = require('../letterPositions');
+const assert = require('chai').assert;
+
+describe('#letterPositions', () => {
+  it(`returns { h: [0], e: [1], l: [2, 3], o: [4] } for 'hello'`, () => {
+    assert.deepEqual(letterPositions('hello'),
+      {
+        h: [0],
+        e: [1],
+        l: [2, 3],
+        o: [4]
+      }
+    );
+  });
+});
