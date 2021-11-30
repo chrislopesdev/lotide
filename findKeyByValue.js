7fc9680abf2a3a9f09a2c3cc18e93cfe7e958622
chris lopes <chris.lopes@gmail.com>
2021-11-30 13:08:37 +0000

    update findKey ByValue mocha chai

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKeyByValue = function(obj, value) {
   for (const key in obj) {
     if (obj[key] === value) {
@@ -13,16 +5,5 @@ const findKeyByValue = function(obj, value) {
     }
   }
 };
-
-// Tests
-
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 
 module.exports = findKeyByValue;
---
+++ test/findKeyByValue.test.js
@@ -0,0 +1,20 @@
+const findKeyByValue = require('../findKeyByValue');
+const assert = require('chai').assert;
+
+describe("#findKeyByValue", () => {
+  it("returns key from object based on given value", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  });
+
+  it("returns undefined if value not present", () => {
+    assert.strictEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+  });
+
+});
+
+// Tests
+// const bestTVShowsByGenre = {
+//   sci_fi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama:  "The Wire"
+// };
