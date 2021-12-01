40b05c3eeca3060a0d04a73ae09b4eb709f1d593
chris lopes <chris.lopes@gmail.com>
2021-12-01 13:12:21 +0000

    countLetters mocha chai

--- countLetters.js
+++ countLetters.js
@@ -12,21 +12,4 @@ const countLetters = function(string) {
   return letterCount;
 };
 
-// TEST CASE
-
-console.log(countLetters('lighthouse in the house'));
-
-// console.log(assertEqual(countLetters('lighthouse in the house'), {
-//   l: 1,
-//   i: 2,
-//   g: 1,
-//   h: 4,
-//   t: 2,
-//   o: 2,
-//   u: 2,
-//   s: 2,
-//   e: 3,
-//   n: 1,
-// }))
-
 module.exports = countLetters;
